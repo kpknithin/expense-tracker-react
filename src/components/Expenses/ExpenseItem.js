0ef954242f568d6afe10f8dda979b9db45a8c35e
Kavinithees P <kavinithees2696@gmail.com>
2021-12-16 08:42:16 +0530

    Added Edit Button

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,15 +3,20 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
+  const clickHandler = () => {
+    console.log("click Event!!!");
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">₹{props.amount}</div>
+        <button className="btn-edit" onClick={clickHandler}>Edit</button>
       </div>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
