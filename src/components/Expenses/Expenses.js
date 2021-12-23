e6adbb88cdf3f869d1f873f835ce56d464c20bb5
Kavinithees P <kavinithees2696@gmail.com>
2021-12-23 08:15:07 +0530

    Added Expense filtered with Two Way binding

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,21 @@
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import { useState } from "react/cjs/react.development";
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2021");
+  const filterExpenses = (selectedYear) => {
+    console.log("Filtered Expenses with Date: " + selectedYear);
+    setFilteredYear(selectedYear);
+  };
   return (
     <Card className="expenses">
+      <ExpensesFilter
+        selectedYear={filteredYear}
+        onFilterExpenses={filterExpenses}
+      />
       <ExpenseItem
         title={props.items[0].title}
         date={props.items[0].date}
