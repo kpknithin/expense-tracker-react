96d6239ce1d6062ac3c01b783da60c262555edcf
Kavinithees P <kavinithees2696@gmail.com>
2021-12-30 15:11:42 +0530

    Assignment solution

--- src/components/Expenses/NewExpense/NewExpense.js
+++ src/components/Expenses/NewExpense/NewExpense.js
@@ -2,17 +2,14 @@ import { useState } from "react/cjs/react.development";
 import ExpenseForm from "./ExpenseForm";
 
 function NewExpense(props) {
-  const[isEditing,setIsEditing] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
   const saveExpenseDataHaldler = (enteredExpenseData) => {
-    
     // Construct the latest ID
     const newExpenseId = new Date().getTime();
     const newExpenseData = {
       ...enteredExpenseData,
       id: "e" + newExpenseId,
     };
-    console.log("New Expense Data: ");
-    console.log(newExpenseData);
 
     // Adding new Expense into existing object
     props.onAddExpense(newExpenseData);
@@ -21,21 +18,22 @@ function NewExpense(props) {
 
   const startEditingHandler = (event) => {
     setIsEditing(true);
-    event.preventDefault();
-  }
+  };
   const stopEditingHandler = (event) => {
     setIsEditing(false);
-    event.preventDefault();
-  }
+  
+  };
+
   return (
     <div className="new-expense">
-      {!isEditing && (
-    
-      <button onClick={startEditingHandler}>Add New Expense</button>
-    )}
-     {isEditing && (
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHaldler} onCancel={stopEditingHandler} />
-     )}
+      {isEditing ? (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHaldler}
+          onCancel={stopEditingHandler}
+        />
+      ) : (
+        <button type="button" onClick={startEditingHandler}>Add New Expense</button>
+      )}
     </div>
   );
 }
